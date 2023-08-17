384b3d9167dda09f3386ede10f46513b5a2bd78f
seung <tmdvy1039@gmail.com>
2023-08-17 12:05:13 +0900

    check func

--- udder/ViewController.swift
+++ udder/ViewController.swift
@@ -8,11 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        <#code#>
+    }
+}
 
-
-}dsfsdfdsfasd
